43d5a120d7a2714dc98321e44c55b40bd8e4fc75
alkn87 <alex@aknapp.at>
2019-01-19 06:39:29 -0800

    changes to filehandle: inserted absolut path in order to get the cronjob running

--- filehandle.py
+++ filehandle.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 
 import sys
+import os
 import time
 from time import localtime, strftime
+
+abspath = "/home/aknapp/Documents/Betriebssysteme/FilesysMem"
 
 def get_current_time():
     time_string = strftime("%a, %d %b %Y %H:%M:%S", localtime())
     return time_string
 
 def write_to_file(filename):
-    f = open(filename,"w+")
+    f = open(abspath + filename,"w+")
     i = 0
     while (i <= 25):
         print("Write Line: " + str(i) + " - " + get_current_time() + "\n")
@@ -19,7 +22,7 @@ def write_to_file(filename):
     f.close()
 
 def append_to_file(filename):
-    f = open(filename, "a+")
+    f = open(abspath + filename, "a+")
     i = 0
     while (i <= 25):
         print("Append Line: " + str(i) + " - " + get_current_time() + "\n")
@@ -29,14 +32,14 @@ def append_to_file(filename):
     f.close()
 
 def read_from_file(filename):
-    f = open(filename, "r")
+    f = open(abspath + filename, "r")
     i = 0
     for n in f.readlines():
         print(n)
         time.sleep(1)
     f.close()
 
-write_to_file("./file1.txt")
-append_to_file("./file2.txt")
-read_from_file("./file1.txt")
-read_from_file("./file2.txt")
+write_to_file("/file1.txt")
+append_to_file("/file2.txt")
+read_from_file("/file1.txt")
+read_from_file("/file2.txt")
